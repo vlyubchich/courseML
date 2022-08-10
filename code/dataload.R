dc285d4db55353d7ba8c98b667b68645866369f6
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2022-08-10 11:08:30 -0300

    load RCA

--- code/dataload.R
+++ code/dataload.R
@@ -6,4 +6,7 @@ str(Db)
 git config --global user.email "lyubchich@umces.edu"
 git config --global user.name "vlyubchich"
 
-# new day
+# 2022-08-10
+Drca <- read.csv("./dataraw/rca_data_2012_2022-06-29.csv",
+                nrows = 10) # here load only 10 top rows
+ls(Drca)
